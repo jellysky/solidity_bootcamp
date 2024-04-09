4f14fc93dddde8b51a0c353e196b05082f65f836
Peter Koushik Mallik <jellysky@users.noreply.github.com>
2024-04-08 22:12:20 -0400

    Delete backend directory

--- backend/src/dtos/mintToken.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class MintTokenDto {
-    @ApiProperty({type: String, required: true, default: process.env.PETER_ADDRESS})
-    address: string;
-}
